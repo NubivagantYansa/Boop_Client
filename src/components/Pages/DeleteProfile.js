d8abe95e4e12d98f931674585bdea297f835e310
“Debora <debora.crescenzo@gmail.com>
2020-11-12 21:00:37 +0000

    delete page style

--- src/components/Pages/DeleteProfile.js
+++ src/components/Pages/DeleteProfile.js
@@ -14,14 +14,23 @@ const DeleteProfile = () => {
   };
   return (
     <>
-      <div className='container delete-background-image'>
-        <p>Are you sure you want to delete your profile?</p>
-        <form onSubmit={handleSubmit}>
-          <button className='btn danger'>Delete</button>
-        </form>
-        <button className='btn info' id='link'>
-          <Link to='/dashboard'>Back</Link>
-        </button>
+      <div className='container delete-background-image p-3 m-3 text-center'>
+        <div className='row justify-content-center p-3 m-3'>
+          <h3 className='display-4'>
+            Are you sure you want to delete your profile?
+          </h3>
+        </div>
+        <div className='row  justify-content-center'>
+          <form onSubmit={handleSubmit}>
+            <button className='m-2 btn danger'>Delete</button>
+          </form>
+
+          <button className='m-2 btn info' id='link'>
+            <Link id='link' to='/dashboard'>
+              Back
+            </Link>
+          </button>
+        </div>
       </div>
     </>
   );
